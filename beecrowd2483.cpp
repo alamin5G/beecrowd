c2702d7d880687df9054a4e9253c1c8b61bcf7a8
alamin5g <alaminrakib4864@gmail.com>
2022-04-13 15:25:39 +0600

    Merry Christmaaas! - https://prnt.sc/dnmxjmb3bfB5

---
+++ beecrowd2483.cpp
@@ -0,0 +1,30 @@
+/**
+ * @file beecrowd2483.cpp
+ * Merry Christmaaas! - https://prnt.sc/dnmxjmb3bfB5
+ * @author Md. Alamin (alamin5g@yahoo.com)
+ * I would love be a software engineer at Google. That is why anybody can uses this code without any condition, if you face any difficulty, then try to email me.
+ * @version 0.1
+ * @date 2022-04-13
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+     
+    int i, j;
+    cin >> i;
+    cout << "Feliz nat";
+    for(j = 0;j < i;j++)
+    {
+        cout << 'a' ;
+    }
+    cout << "l!" << endl;
+
+    return 0;
+}
