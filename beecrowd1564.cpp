8dcd026c36dc30886c97dd89fd028584976ab80c
alamin5g <alaminrakib4864@gmail.com>
2022-04-22 02:35:40 +0600

    Brazil World Cup - https://prnt.sc/KaIvx70hkzNT

---
+++ beecrowd1564.cpp
@@ -0,0 +1,34 @@
+/**
+ * @file beecrowd1564.cpp
+ * Brazil World Cup - https://prnt.sc/KaIvx70hkzNT
+ * @author Md. Alamin (alamin5g@yahoo.com)
+ * I would love be a software engineer at Google. That is why anybody can uses this code without any condition, if you face any difficulty, then try to email me.
+ * @version 0.1
+ * @date 2022-04-22
+ *
+ * @copyright Copyright (c) 2022
+ *
+ */
+
+#include <bits/stdc++.h>
+#include <string.h>
+
+using namespace std;
+
+int main()
+{
+    int n;
+    while (scanf("%d", &n)!= EOF )
+    {
+        if (n == 0)
+        {
+            cout << "vai ter copa!" << endl;
+        }
+        else if (n > 0)
+        {
+            cout << "vai ter duas!" << endl;
+        }
+    }
+
+    return 0;
+}
