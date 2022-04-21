d79baece2201368c8a8ca9a5d17d63dc7d1aac83
alamin5g <alaminrakib4864@gmail.com>
2022-04-22 01:36:19 +0600

    The Output - https://prnt.sc/hXNnMksMZBrL

---
+++ beecrowd2861.cpp
@@ -0,0 +1,29 @@
+/**
+ * @file beecrowd2861.cpp
+ * The Output - https://prnt.sc/hXNnMksMZBrL
+ * @author Md. Alamin (alamin5g@yahoo.com)
+ * I would love be a software engineer at Google. That is why anybody can uses this code without any condition, if you face any difficulty, then try to email me.
+ * @version 0.1
+ * @date 2022-04-22
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
+
+#include <bits/stdc++.h>
+#include <string.h>
+
+using namespace std;
+
+int main()
+{
+    int c;
+    cin >> c; 
+    string q, a;
+    while(c--){
+        getline(cin, q);
+        cout << "gzuz" <<endl;
+    }
+    
+    return 0;
+}
