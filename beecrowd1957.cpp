d35305770ae920b25f520f6a2f89173da965d523
Md Al Amin <alaminrakib4864@gmail.com>
2022-04-24 08:23:10 +0600

    convert hexa

---
+++ beecrowd1957.cpp
@@ -0,0 +1,21 @@
+/**
+ * @file beecrowd1957.cpp
+ * Converting to hexa decimal
+ * @author Md. Alamin (alamin5g@yahoo.com)
+ * I would love be a software engineer at Google. That is why anybody can uses this code without any condition, if you face any difficulty, then try to email me.
+ * @version 0.1
+ * @date 2022-04-24
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    int d;
+    
+    return 0;
+}
