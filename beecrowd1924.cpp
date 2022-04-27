377ef5705f2294eb4acbe0b7bf322c6964cd7366
Md Al Amin <alaminrakib4864@gmail.com>
2022-04-27 12:17:39 +0600

    Vitória and Her Indecision

---
+++ beecrowd1924.cpp
@@ -0,0 +1,26 @@
+/**
+ * @file beecrowd1924.cpp
+ * Vitória and Her Indecision
+ * @author Md. Alamin (alamin5g@yahoo.com)
+ * I would love be a software engineer at Google. That is why anybody can uses this code without any condition, if you face any difficulty, then try to email me.
+ * @version 0.1
+ * @date 2022-04-27
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    int n;
+    cin >> n;
+    char m[100];
+    for(auto i=0; i<n; i++){
+        cin >> m;
+    }
+
+    cout << "Ciencia da Computacao" << endl;
+
+    return 0;
+}
