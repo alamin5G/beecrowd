e3c2384b1d15eb157ac385744fd800261f05a630
Md Al Amin <alaminrakib4864@gmail.com>
2022-04-27 07:03:42 +0600

    Converting to hexa decimal

--- beecrowd1957.cpp
+++ beecrowd1957.cpp
@@ -15,7 +15,39 @@
 using namespace std;
 
 int main(){
-    int d;
-    
+    long long int d;
+    cin >> d;
+    long long int remain;
+    int i = 0;
+    char hex[19];
+
+    while(d!=0){
+        remain = d%16;
+        d = d/16;
+        if(remain<=9){
+            hex[i] = remain+48;
+        }else if(remain == 10)
+        {
+            hex[i] = remain + 55; // 10+55=65 (65=A)
+        }else if(remain == 11){
+            hex[i] = remain + 55; //11+55 = 66 (66=B)
+        }else if(remain == 12 ){
+            hex[i] = remain + 55;
+        }else if(remain == 13 ){
+            hex[i] = remain + 55;
+        }else if(remain == 14 ){
+            hex[i] = remain + 55;
+        }else if(remain == 15 ){
+            hex[i] = remain + 55;
+        }
+        i++;
+    }
+
+    hex[i] = '\0';
+    int len = strlen(hex);
+    for(i=len-1; i>=0; i--){
+        cout << hex[i];
+    }
+    cout << "\n";
     return 0;
 }
