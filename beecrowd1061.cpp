24cd672ef3212da427664da3b502211088cc2fa1
Md. Alamin <alaminrakib4864@gmail.com>
2022-06-28 09:08:23 +0600

    1061 beecrowd

---
+++ beecrowd1061.cpp
@@ -0,0 +1,23 @@
+/**
+ * @file beecrowd1061.cpp
+ * see the problem
+ * @author Md. Alamin (alamin5g@yahoo.com)
+ * I would love be a software engineer at Google. That is why anybody can uses this code without any condition, if you face any difficulty, then try to email me.
+ * @version 0.1
+ * @date 2022-06-28
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    string temp;
+    int dayStart, startHour, startMin, startSec;
+    int dayEnd, endHour, endMin, endSec;
+    int day, hour, min, sec;
+
+    cin >> temp >> dayStart;
+    cin >> startHour>> startMin >> startSec;
+    return 0;
+}
