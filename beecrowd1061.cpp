625d4f1c7d66a7f003e98c9a3af0a49815edffbd
Md. Alamin <alaminrakib4864@gmail.com>
2022-06-28 21:25:34 +0600

    problem 1061

--- beecrowd1061.cpp
+++ beecrowd1061.cpp
@@ -12,12 +12,41 @@
 #include <bits/stdc++.h>
 using namespace std;
 int main(){
+
     string temp;
     int dayStart, startHour, startMin, startSec;
     int dayEnd, endHour, endMin, endSec;
-    int day, hour, min, sec;
 
     cin >> temp >> dayStart;
-    cin >> startHour>> startMin >> startSec;
+    cin >> startHour>> temp >> startMin >> temp >> startSec;
+    cin >> temp >> dayEnd;
+    cin >> endHour >> temp >> endMin >> temp >> endSec;
+
+
+    startSec = endSec - startSec;
+    startMin = endMin - startMin;
+    startHour = endHour - startHour;
+    dayStart = dayEnd - dayStart;
+
+    if(startSec<0){
+        startSec += 60;
+        startMin--;
+    }
+
+    if(startMin<0){
+        startMin+=60;
+        startHour--;
+    }
+
+    if(startHour <0){
+        startHour+=24;
+        dayStart--;
+        
+    }
+    cout << dayStart << " dia(s)" << endl;
+    cout << startHour << " hora(s)" << endl;
+    cout << startMin << " minuto(s)" << endl;
+    cout << startSec << " segundo(s)" << endl;
+
     return 0;
 }
