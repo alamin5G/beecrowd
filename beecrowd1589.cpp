7e4285503e5692bb04755c8fb9be1e8fccf15bdc
Md. Alamin <alaminrakib4864@gmail.com>
2022-07-05 02:32:07 +0600

    see the problem - https://prnt.sc/4sbPjqqrFJuW

---
+++ beecrowd1589.cpp
@@ -0,0 +1,24 @@
+/**
+ * @file beecrowd1589.cpp
+ * see the problem - https://prnt.sc/4sbPjqqrFJuW
+ * @author Md. Alamin (alamin5g@yahoo.com)
+ * I would love be a software engineer at Google. That is why anybody can uses this code without any condition, if you face any difficulty, then try to email me.
+ * @version 0.1
+ * @date 2022-07-05
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    int t; 
+    cin >> t;
+    while(t--){
+        int a, b;
+        
+        cin >> a >> b;
+        cout << a + b << endl;
+    }
+    return 0;
+}
