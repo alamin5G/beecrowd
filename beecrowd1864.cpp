e84ddd55df4da7084fd0337981135c491c83b617
Md. Alamin <alaminrakib4864@gmail.com>
2022-07-05 03:34:06 +0600

    see the problem - https://prnt.sc/A2gQoUI15CHw

---
+++ beecrowd1864.cpp
@@ -0,0 +1,24 @@
+/**
+ * @file beecrowd1864.cpp
+ * see the problem - https://prnt.sc/A2gQoUI15CHw
+ * @author Md. Alamin (alamin5g@yahoo.com)
+ * I would love be a software engineer at Google. That is why anybody can uses this code without any condition, if you face any difficulty, then try to email me.
+ * @version 0.1
+ * @date 2022-07-05
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    string quote = "LIFE IS NOT A PROBLEM TO BE SOLVED";
+    int n;
+    cin >> n;
+    for(auto i=0; i<n; i++){
+        cout << quote[i];
+    }
+    cout << endl;
+
+    return 0;
+}
