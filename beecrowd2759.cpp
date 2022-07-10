a999fa08cea2a28886be042a622fb83f63144b75
Md. Alamin <alaminrakib4864@gmail.com>
2022-07-10 17:36:41 +0600

    see the problem - https://prnt.sc/pjO87v0MhKPv

---
+++ beecrowd2759.cpp
@@ -0,0 +1,21 @@
+/**
+ * @file beecrowd2759.cpp
+ * see the problem - https://prnt.sc/pjO87v0MhKPv
+ * @author Md. Alamin (alamin5g@yahoo.com)
+ * I would love be a software engineer at Google. That is why anybody can uses this code without any condition, if you face any difficulty, then try to email me.
+ * @version 0.1
+ * @date 2022-07-10
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    char a, b, c;
+    cin >> a >> b >> c;
+    cout << "A = " << a << ", B = " << b << ", C = " << c << endl;
+    cout << "A = " << b << ", B = " << c << ", C = " << a << endl;
+    cout << "A = " << c << ", B = " << a << ", C = " << b << endl;
+    return 0;
+}
