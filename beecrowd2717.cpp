2e57e3da066dd77f4e9b750d948d2084a703c8e6
Md. Alamin <alaminrakib4864@gmail.com>
2022-08-13 21:30:57 +0600

    see the problem - https://prnt.sc/z74yNoID-d57

---
+++ beecrowd2717.cpp
@@ -0,0 +1,27 @@
+/**
+ * @file beecrowd2717.cpp
+ * see the problem - https://prnt.sc/z74yNoID-d57
+ * @author Md. Alamin (alamin5g@yahoo.com)
+ * I would love be a software engineer at Google. That is why anybody can uses this code without any condition, if you face any difficulty, then try to email me.
+ * @version 0.1
+ * @date 2022-08-13
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    int n;
+    cin >> n;
+    int a , b;
+    cin >> a >> b;
+    if((a+b)<=n){
+        cout << "Farei hoje!" << endl;
+    }else{
+        cout << "Deixa para amanha!" << endl;
+    }
+    
+
+    return 0;
+}
