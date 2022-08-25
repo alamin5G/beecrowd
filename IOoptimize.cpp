37f8f8989ec1ef03ba4c489574e8cd5eec2b3ec6
Md. Alamin <alaminrakib4864@gmail.com>
2022-08-25 23:26:05 +0600

    input output optimize

--- IOoptimize.cpp
+++ IOoptimize.cpp
@@ -12,6 +12,7 @@
 #include <bits/stdc++.h>
 using namespace std;
 #define optimize() ios_base::sync_with_stdio(0); cin.tie(0);cout.tie(0);
+#define endl "\n";
 int main(){
     optimize();
     int n;
